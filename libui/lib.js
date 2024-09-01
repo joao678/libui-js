4cab69dc5ec7874548086f4b0dacff61020c2f70
fcanudo <joao678@gmail.com>
2024-09-01 09:08:38 -0300

    basic linux support

--- libui/lib.js
+++ libui/lib.js
@@ -3,7 +3,19 @@ const path = require('path');
 const fs = require('fs');
 const os = require('os');
 
-let libui_lib = './libs/libui.dll';
+let libui_lib = '';
+switch (process.platform) {
+    case 'win32':
+        libui_lib = `./libs/libui.dll`;
+        break;
+    case 'linux':
+        libui_lib = `./libs/libui.so`;
+        break;
+    case 'darwin':
+        libui_lib = `./libs/libui.dylib`;
+        break;
+}
+;
 
 if (process.pkg) {
     fs.copyFileSync(path.resolve(__dirname, '../libs/libui.dll'), path.join(os.tmpdir(), '/libui.dll'));
